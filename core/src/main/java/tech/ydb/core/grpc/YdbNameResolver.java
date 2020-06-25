55785d140efe9e504b7881320e6bf02efa6e3cbd
aozeritsky <aozeritsky@yandex-team.ru>
2020-06-25 21:49:26 +0300

    Style
    Note: mandatory check (NEED_CHECK) was skipped
    ref:7037377
    sync: https://proxy.sandbox.yandex-team.ru/1578889663

--- core/src/main/java/tech/ydb/core/grpc/YdbNameResolver.java
+++ core/src/main/java/tech/ydb/core/grpc/YdbNameResolver.java
@@ -8,8 +8,6 @@ import java.net.UnknownHostException;
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 import java.util.function.BiConsumer;
 import java.util.logging.Level;
