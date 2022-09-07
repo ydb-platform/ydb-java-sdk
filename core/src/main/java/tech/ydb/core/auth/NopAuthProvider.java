6e455bc4596faca00d04aca1d5704084a689401a
Alexandr Gorshenin <alex_gorshenin@mail.ru>
2022-09-07 11:11:24 +0300

    New api of AuthProvider

--- core/src/main/java/tech/ydb/core/auth/NopAuthProvider.java
+++ core/src/main/java/tech/ydb/core/auth/NopAuthProvider.java
@@ -4,14 +4,22 @@ package tech.ydb.core.auth;
  * @author Sergey Polovko
  */
 public class NopAuthProvider implements AuthProvider {
-
     public static final NopAuthProvider INSTANCE = new NopAuthProvider();
 
     private NopAuthProvider() {
     }
 
     @Override
-    public String getToken() {
-        return "";
+    public AuthIdentity createAuthIdentity(AuthRpc rpc) {
+        return EmptyIdentity.INSTANCE;
+    }
+
+    private static final class EmptyIdentity implements AuthIdentity {
+        private static final EmptyIdentity INSTANCE = new EmptyIdentity();
+
+        @Override
+        public String getToken() {
+            return "";
+        }
     }
 }
