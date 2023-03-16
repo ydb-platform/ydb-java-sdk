fb819a7b5d1607e33350e2ca0ff983046b963b8c
KirillKurdyukov <kurdyukov_kirya@mail.ru>
2023-03-16 14:18:33 +0300

    fix code climate

--- core/src/main/java/tech/ydb/core/impl/pool/EndpointPriorityFactory.java
+++ core/src/main/java/tech/ydb/core/impl/pool/EndpointPriorityFactory.java
@@ -14,6 +14,7 @@ import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Ticker;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import tech.ydb.core.grpc.BalancingSettings;
 import tech.ydb.discovery.DiscoveryProtos;
 
