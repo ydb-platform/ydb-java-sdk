f943eda72f520f2b6694b684a11e7244bc788acd
KirillKurdyukov <kurdyukov_kirya@mail.ru>
2023-04-18 12:02:15 +0300

    implement coordination client

--- core/src/main/java/tech/ydb/core/impl/BaseGrpcTransport.java
+++ core/src/main/java/tech/ydb/core/impl/BaseGrpcTransport.java
@@ -38,7 +38,7 @@ public abstract class BaseGrpcTransport implements GrpcTransport {
 
     private final OperationManager operationManager = new OperationManager(this);
 
-    private volatile boolean shutdown = false;
+    protected volatile boolean shutdown = false;
 
     public abstract AuthCallOptions getAuthCallOptions();
 
