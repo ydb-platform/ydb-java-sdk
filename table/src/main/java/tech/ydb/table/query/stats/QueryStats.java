3a259a18150c98923cb56b4ee3bf013f706e7770
Michael B <bagrintsev22@gmail.com>
2023-07-31 15:08:50 +0300

    remove setters in stats class

--- table/src/main/java/tech/ydb/table/query/stats/QueryStats.java
+++ table/src/main/java/tech/ydb/table/query/stats/QueryStats.java
@@ -3,6 +3,7 @@ package tech.ydb.table.query.stats;
 import java.io.Serializable;
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 public final class QueryStats implements Serializable {
@@ -10,8 +11,8 @@ public final class QueryStats implements Serializable {
     private final List<QueryPhaseStats> queryPhases;
     private CompilationStats compilation;
     private long processCpuTimeUs;
-    private String queryPlan;
-    private String queryAst;
+    private final String queryPlan;
+    private final String queryAst;
     private long totalDurationUs;
     private long totalCpuTimeUs;
     private int memoizedHashCode;
@@ -57,33 +58,26 @@ public final class QueryStats implements Serializable {
         return this.compilation == null ? CompilationStats.getDefaultInstance() : this.compilation;
     }
 
-    public void setCompilation(CompilationStats compilation) {
-        this.compilation = compilation;
-    }
-
     public long getProcessCpuTimeUs() {
         return this.processCpuTimeUs;
-    }
-
-    public void setProcessCpuTimeUs(long processCpuTimeUs) {
-        this.processCpuTimeUs = processCpuTimeUs;
     }
 
     public long getTotalDurationUs() {
         return this.totalDurationUs;
     }
 
-    public void setTotalDurationUs(long totalDurationUs) {
-        this.totalDurationUs = totalDurationUs;
-    }
-
     public long getTotalCpuTimeUs() {
         return this.totalCpuTimeUs;
     }
 
-    public void setTotalCpuTimeUs(long totalCpuTimeUs) {
-        this.totalCpuTimeUs = totalCpuTimeUs;
+    public String getQueryAst() {
+        return queryAst;
     }
+
+    public String getQueryPlan() {
+        return queryPlan;
+    }
+
 
     @Override
     public boolean equals(Object obj) {
@@ -93,71 +87,24 @@ public final class QueryStats implements Serializable {
             return super.equals(obj);
         } else {
             QueryStats other = (QueryStats) obj;
-            if (!this.getQueryPhasesList().equals(other.getQueryPhasesList())) {
-                return false;
-            } else if (this.hasCompilation() != other.hasCompilation()) {
-                return false;
-            } else if (this.hasCompilation() && !this.getCompilation().equals(other.getCompilation())) {
-                return false;
-            } else if (this.getProcessCpuTimeUs() != other.getProcessCpuTimeUs()) {
-                return false;
-            } else if (!this.getQueryPlan().equals(other.getQueryPlan())) {
-                return false;
-            } else if (!this.getQueryAst().equals(other.getQueryAst())) {
-                return false;
-            } else if (this.getTotalDurationUs() != other.getTotalDurationUs()) {
-                return false;
-            } else {
-                return this.getTotalCpuTimeUs() == other.getTotalCpuTimeUs();
-            }
+            return Objects.equals(getQueryPhasesList(), other.getQueryPhasesList()) &&
+                    Objects.equals(getCompilation(), other.getCompilation()) &&
+                    Objects.equals(getProcessCpuTimeUs(), other.getProcessCpuTimeUs()) &&
+                    Objects.equals(getQueryPlan(), other.getQueryPlan()) &&
+                    Objects.equals(getQueryAst(), other.getQueryAst()) &&
+                    Objects.equals(getTotalDurationUs(), other.getTotalDurationUs()) &&
+                    Objects.equals(getTotalCpuTimeUs(), other.getTotalCpuTimeUs());
         }
     }
 
     @Override
     public int hashCode() {
-        if (this.memoizedHashCode != 0) {
-            return this.memoizedHashCode;
-        } else {
-            int hash = 41;
-            if (this.getQueryPhasesCount() > 0) {
-                hash = 37 * hash + 1;
-                hash = 53 * hash + this.getQueryPhasesList().hashCode();
-            }
-
-            if (this.hasCompilation()) {
-                hash = 37 * hash + 2;
-                hash = 53 * hash + this.getCompilation().hashCode();
-            }
-
-            hash = 37 * hash + 3;
-            hash = 53 * hash + Long.hashCode(this.getProcessCpuTimeUs());
-            hash = 37 * hash + 4;
-            hash = 53 * hash + this.getQueryPlan().hashCode();
-            hash = 37 * hash + 5;
-            hash = 53 * hash + this.getQueryAst().hashCode();
-            hash = 37 * hash + 6;
-            hash = 53 * hash + Long.hashCode(this.getTotalDurationUs());
-            hash = 37 * hash + 7;
-            hash = 53 * hash + Long.hashCode(this.getTotalCpuTimeUs());
-            this.memoizedHashCode = hash;
-            return hash;
+        if (this.memoizedHashCode == 0) {
+            this.memoizedHashCode =
+                    Objects.hash(getQueryPhasesList(), getCompilation(), getProcessCpuTimeUs(), getQueryPlan(),
+                            getQueryAst(), getTotalDurationUs(), getTotalCpuTimeUs());
         }
-    }
-
-    public String getQueryAst() {
-        return queryAst;
-    }
-
-    public void setQueryAst(String queryAst) {
-        this.queryAst = queryAst;
-    }
-
-    public String getQueryPlan() {
-        return queryPlan;
-    }
-
-    public void setQueryPlan(String queryPlan) {
-        this.queryPlan = queryPlan;
+        return this.memoizedHashCode;
     }
 
     @Override
--- table/src/main/java/tech/ydb/table/query/stats/TableAccessStats.java
+++ table/src/main/java/tech/ydb/table/query/stats/TableAccessStats.java
@@ -1,5 +1,7 @@
 package tech.ydb.table.query.stats;
 
+import java.util.Objects;
+
 public final class TableAccessStats {
     private static final TableAccessStats DEFAULT_INSTANCE = new TableAccessStats();
     private String name;
@@ -20,7 +22,7 @@ public final class TableAccessStats {
         this.partitionsCount = protoAutoGenTableAccessStats.getPartitionsCount();
     }
 
-    public static TableAccessStats getDefaultInstance() {
+    private static TableAccessStats getDefaultInstance() {
         return DEFAULT_INSTANCE;
     }
 
@@ -28,52 +30,20 @@ public final class TableAccessStats {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public boolean hasReads() {
-        return this.reads != null;
-    }
-
     public OperationStats getReads() {
         return this.reads == null ? OperationStats.getDefaultInstance() : this.reads;
-    }
-
-    public void setReads(OperationStats reads) {
-        this.reads = reads;
-    }
-
-    public boolean hasUpdates() {
-        return this.updates != null;
     }
 
     public OperationStats getUpdates() {
         return this.updates == null ? OperationStats.getDefaultInstance() : this.updates;
     }
 
-    public void setUpdates(OperationStats updates) {
-        this.updates = updates;
-    }
-
-    public boolean hasDeletes() {
-        return this.deletes != null;
-    }
-
     public OperationStats getDeletes() {
         return this.deletes == null ? OperationStats.getDefaultInstance() : this.deletes;
     }
 
-    public void setDeletes(OperationStats deletes) {
-        this.deletes = deletes;
-    }
-
     public long getPartitionsCount() {
         return this.partitionsCount;
-    }
-
-    public void setPartitionsCount(long partitionsCount) {
-        this.partitionsCount = partitionsCount;
     }
 
     @Override
@@ -84,54 +54,20 @@ public final class TableAccessStats {
             return super.equals(obj);
         } else {
             TableAccessStats other = (TableAccessStats) obj;
-            if (!this.getName().equals(other.getName())) {
-                return false;
-            } else if (this.hasReads() != other.hasReads()) {
-                return false;
-            } else if (this.hasReads() && !this.getReads().equals(other.getReads())) {
-                return false;
-            } else if (this.hasUpdates() != other.hasUpdates()) {
-                return false;
-            } else if (this.hasUpdates() && !this.getUpdates().equals(other.getUpdates())) {
-                return false;
-            } else if (this.hasDeletes() != other.hasDeletes()) {
-                return false;
-            } else if (this.hasDeletes() && !this.getDeletes().equals(other.getDeletes())) {
-                return false;
-            } else {
-                return this.getPartitionsCount() == other.getPartitionsCount();
-            }
+            return Objects.equals(getName(), other.getName()) && Objects.equals(getReads(), other.getReads()) &&
+                    Objects.equals(getUpdates(), other.getUpdates()) &&
+                    Objects.equals(getDeletes(), other.getDeletes()) &&
+                    Objects.equals(getPartitionsCount(), other.getPartitionsCount());
         }
     }
 
     @Override
     public int hashCode() {
-        if (this.memoizedHashCode != 0) {
-            return this.memoizedHashCode;
-        } else {
-            int hash = 41;
-            hash = 37 * hash + 1;
-            hash = 53 * hash + this.getName().hashCode();
-            if (this.hasReads()) {
-                hash = 37 * hash + 3;
-                hash = 53 * hash + this.getReads().hashCode();
-            }
-
-            if (this.hasUpdates()) {
-                hash = 37 * hash + 4;
-                hash = 53 * hash + this.getUpdates().hashCode();
-            }
-
-            if (this.hasDeletes()) {
-                hash = 37 * hash + 5;
-                hash = 53 * hash + this.getDeletes().hashCode();
-            }
-
-            hash = 37 * hash + 6;
-            hash = 53 * hash + Long.hashCode(this.getPartitionsCount());
-            this.memoizedHashCode = hash;
-            return hash;
+        if (this.memoizedHashCode == 0) {
+            this.memoizedHashCode =
+                    Objects.hash(getName(), getReads(), getUpdates(), getDeletes(), getPartitionsCount());
         }
+        return this.memoizedHashCode;
     }
 
     @Override
