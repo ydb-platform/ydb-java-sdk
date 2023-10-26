227588b391e09faae770efd9f40979395c3750d8
Nikolay Perfilov <pnv1@yandex-team.ru>
2023-10-26 14:39:50 +0300

    Add DeferredCommitter factory method

--- topic/src/main/java/tech/ydb/topic/read/DeferredCommitter.java
+++ topic/src/main/java/tech/ydb/topic/read/DeferredCommitter.java
@@ -1,5 +1,8 @@
 package tech.ydb.topic.read;
 
+import tech.ydb.topic.read.events.DataReceivedEvent;
+import tech.ydb.topic.read.impl.DeferredCommitterImpl;
+
 /**
  * A helper class that is used to call deferred commits.
  * Several {@link Message}s or/and {@link tech.ydb.topic.read.events.DataReceivedEvent}s can be accepted to commit later
@@ -11,6 +14,15 @@ package tech.ydb.topic.read;
  */
 public interface DeferredCommitter {
     /**
+     * Creates a new instance of {@link DeferredCommitter}
+     *
+     * @return a new instance of {@link DeferredCommitter}
+     */
+    static DeferredCommitter newInstance() {
+        return new DeferredCommitterImpl();
+    }
+
+    /**
      * Adds a {@link Message} to commit it later with a commit method
      *
      * @param message a {@link Message} to commit later
@@ -18,7 +30,14 @@ public interface DeferredCommitter {
     void add(Message message);
 
     /**
-     * Commits offset ranges from all {@link Message}s and {@link tech.ydb.topic.read.events.DataReceivedEvent}s
+     * Adds a {@link DataReceivedEvent} to commit all its messages later with a commit method
+     *
+     * @param event a {@link DataReceivedEvent} to commit later
+     */
+    void add(DataReceivedEvent event);
+
+    /**
+     * Commits offset ranges from all {@link Message}s and {@link DataReceivedEvent}s
      * that were added to this DeferredCommitter since last commit
      */
     void commit();
