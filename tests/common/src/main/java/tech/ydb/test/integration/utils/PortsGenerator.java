1403760fd28fe20989619abce5f113eae07513de
Nikolay Firov <thergfire@gmail.com>
2024-04-23 10:48:36 +0200

    Taking provided by system port for test containers

--- tests/common/src/main/java/tech/ydb/test/integration/utils/PortsGenerator.java
+++ tests/common/src/main/java/tech/ydb/test/integration/utils/PortsGenerator.java
@@ -9,37 +9,16 @@ import java.util.Random;
 import javax.net.ServerSocketFactory;
 
 /**
- *
  * @author Alexandr Gorshenin
  */
 public class PortsGenerator {
-    private static final int PORT_RANGE_MIN = 51000;
-    private static final int PORT_RANGE_MAX = 59000;
-    private static final Random RANDOM = new SecureRandom();
-
-    private int nextPort;
-
-    public PortsGenerator() {
-        this.nextPort = randomPort();
-    }
-
     public int findAvailablePort() {
-        while (true) {
-            nextPort++;
-            if (nextPort > PORT_RANGE_MAX) {
-                nextPort = randomPort();
-            }
-
-            try {
-                ServerSocket serverSocket = ServerSocketFactory.getDefault()
-                        .createServerSocket(nextPort, 1, InetAddress.getByName("localhost"));
-                serverSocket.close();
-                return nextPort;
-            } catch (IOException ex) { } // port is used
+        try {
+            ServerSocket serverSocket = ServerSocketFactory.getDefault().createServerSocket();
+            serverSocket.close();
+            return serverSocket.getLocalPort();
+        } catch (IOException ex) {
+            throw new RuntimeException("Can't find available port", ex);
         }
-    }
-
-    private static int randomPort() {
-        return PORT_RANGE_MIN + RANDOM.nextInt(PORT_RANGE_MAX - PORT_RANGE_MIN + 1);
     }
 }
