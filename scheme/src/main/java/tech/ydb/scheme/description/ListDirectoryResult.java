54dff80af53a8aa956fb70ecf62f8320072004ce
Alexandr Gorshenin <alexandr268@ydb.tech>
2025-03-10 13:25:31 +0000

    Added more userfriendly api for Entry

--- scheme/src/main/java/tech/ydb/scheme/description/ListDirectoryResult.java
+++ scheme/src/main/java/tech/ydb/scheme/description/ListDirectoryResult.java
@@ -1,6 +1,7 @@
 package tech.ydb.scheme.description;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import tech.ydb.proto.scheme.SchemeOperationProtos;
 
@@ -10,13 +11,20 @@ import tech.ydb.proto.scheme.SchemeOperationProtos;
 public class ListDirectoryResult extends DescribePathResult {
 
     private final List<SchemeOperationProtos.Entry> children;
+    private final List<Entry> entryChildren;
 
     public ListDirectoryResult(SchemeOperationProtos.ListDirectoryResult result) {
         super(result.getSelf());
         this.children = result.getChildrenList();
+        this.entryChildren = result.getChildrenList().stream().map(Entry::new).collect(Collectors.toList());
     }
 
+    @Deprecated
     public List<SchemeOperationProtos.Entry> getChildren() {
         return children;
+    }
+
+    public List<Entry> getEntryChildren() {
+        return entryChildren;
     }
 }
