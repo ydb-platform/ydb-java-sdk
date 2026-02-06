ecc6e087c9453cfce94c26d53bc5bd2c4615250a
Alexandr Gorshenin <alexandr268@ydb.tech>
2026-02-06 16:18:47 +0000

    Fixed ResultSetReader inconsistency with next()/setRowIndex()

--- table/src/main/java/tech/ydb/table/result/impl/ProtoValueReaders.java
+++ table/src/main/java/tech/ydb/table/result/impl/ProtoValueReaders.java
@@ -20,6 +20,7 @@ public class ProtoValueReaders {
         return new ProtoResultSetReader(resultSet);
     }
 
+    @Deprecated
     public static ResultSetReader forResultSets(Collection<ResultSetReader> resultSets) {
         // TODO: add lightweight implementation instead of proto joining
         Preconditions.checkArgument(!resultSets.isEmpty(), "Expect multiple result sets to join from");
