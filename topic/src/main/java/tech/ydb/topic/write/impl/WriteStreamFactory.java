5b5ad03430f3c062929a780ddf0bd2bce1b5faa8
Alexandr Gorshenin <alexandr268@ydb.tech>
2026-04-24 12:18:45 +0100

    Added logs to WriteStreamFactory

--- topic/src/main/java/tech/ydb/topic/write/impl/WriteStreamFactory.java
+++ topic/src/main/java/tech/ydb/topic/write/impl/WriteStreamFactory.java
@@ -2,6 +2,9 @@ package tech.ydb.topic.write.impl;
 
 import java.util.concurrent.CompletableFuture;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import tech.ydb.core.Issue;
 import tech.ydb.core.Result;
 import tech.ydb.core.Status;
@@ -23,6 +26,8 @@ import tech.ydb.topic.settings.WriterSettings;
  * @author Aleksandr Gorshenin
  */
 public class WriteStreamFactory {
+    private final static Logger logger = LoggerFactory.getLogger(WriteStreamFactory.class);
+
     private final String topicPath;
     private final StreamWriteMessage.InitRequest initRequest;
     protected final TopicRpc rpc;
@@ -67,13 +72,15 @@ public class WriteStreamFactory {
                 .build();
     }
 
-    protected Result<Integer> lookupNodeId(long partitionId) {
+    protected Result<Integer> lookupNodeId(String id, long partitionId) {
+        logger.info("[{}] describe topic {} to look up node for partition {}", id, topicPath, partitionId);
         Result<DescribeTopicResult> describeTopic = rpc.describeTopic(
                 DescribeTopicRequest.newBuilder().setIncludeLocation(true).setPath(topicPath).build(),
                 GrpcRequestSettings.newBuilder().build()
         ).join();
 
         if (!describeTopic.isSuccess()) {
+            logger.warn("[{}] describe topic {} failed with status {}", id, topicPath, describeTopic.getStatus());
             return Result.fail(describeTopic.getStatus());
         }
 
@@ -84,40 +91,56 @@ public class WriteStreamFactory {
             }
         }
 
+        logger.warn("[{}] topic {} doesn't have partition {}, direct writing failed", id, topicPath, partitionId);
         Issue issue = Issue.of("Cannot find partition " + partitionId, Issue.Severity.ERROR);
         return Result.fail(Status.of(StatusCode.BAD_REQUEST, issue));
     }
 
-    protected Result<Long> lookupPartitionId(String traceId, String producerId) {
+    protected Result<Long> lookupPartitionId(String id, String producerId) {
         CompletableFuture<Result<Long>> partitionId = new CompletableFuture<>();
 
         // create one-shot stream to detect partitionID for this producer
-        GrpcReadWriteStream<FromServer, FromClient> stream = rpc.writeSession(traceId);
+        logger.info("[{}] create probe stream for topic {} with producer {}", id, topicPath, producerId);
+        GrpcReadWriteStream<FromServer, FromClient> stream = rpc.writeSession(id);
 
         CompletableFuture<Status> streamFuture = stream.start(resp -> {
             if (resp.getStatus() != StatusCodesProtos.StatusIds.StatusCode.SUCCESS) {
                 Status status = Status.of(StatusCode.fromProto(resp.getStatus()), Issue.fromPb(resp.getIssuesList()));
+                logger.warn("[{}] probe stream to topic {} with producer {} got error {}", id, topicPath,
+                        producerId, status);
                 partitionId.complete(Result.fail(status));
                 return;
             }
 
             if (resp.hasInitResponse()) {
-                partitionId.complete(Result.success(resp.getInitResponse().getPartitionId()));
+                long pid = resp.getInitResponse().getPartitionId();
+                logger.warn("[{}] probe stream to topic {} with producer {} has partition {}", id, topicPath,
+                        producerId, pid);
+                partitionId.complete(Result.success(pid));
                 return;
             }
+
+            logger.warn("[{}] probe stream to topic {} with producer {} got unexpexted message {}", id, topicPath,
+                    producerId, resp.getClass().getName());
 
             Issue issue = Issue.of("Unexpected message from stream with producer " + producerId, Issue.Severity.ERROR);
             partitionId.complete(Result.fail(Status.of(StatusCode.BAD_REQUEST, issue)));
         });
 
         if (streamFuture.isDone()) {
+            logger.warn("[{}] probe stream to topic {} with producer {} failed with status {}", id, topicPath,
+                    producerId, streamFuture.join());
             return Result.fail(streamFuture.join());
         }
 
         try {
             streamFuture.whenComplete((st, th) -> {
                 Status status = st != null ? st : Status.of(StatusCode.CLIENT_INTERNAL_ERROR, th);
-                partitionId.complete(Result.fail(status));
+                if (!partitionId.isDone()) {
+                    logger.warn("[{}] probe stream to topic {} with producer {} failed with status {}", id, topicPath,
+                        producerId, streamFuture.join());
+                    partitionId.complete(Result.fail(status));
+                }
             });
             stream.sendNext(initRequest());
             return partitionId.join();
@@ -154,7 +177,7 @@ public class WriteStreamFactory {
 
         @Override
         public WriteSession.Stream createNewStream(String id) {
-            Result<Integer> nodeId = lookupNodeId(partitionId);
+            Result<Integer> nodeId = lookupNodeId(id, partitionId);
             if (!nodeId.isSuccess()) {
                 return new WriteStream.Fail(id, nodeId.getStatus());
             }
@@ -178,7 +201,7 @@ public class WriteStreamFactory {
                 return new WriteStream.Fail(id, partitionId.getStatus());
             }
 
-            Result<Integer> nodeId = lookupNodeId(partitionId.getValue());
+            Result<Integer> nodeId = lookupNodeId(id, partitionId.getValue());
             if (!nodeId.isSuccess()) {
                 return new WriteStream.Fail(id, nodeId.getStatus());
             }
